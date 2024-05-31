a44d957908d02052710a8b26b5b1c17c4bc7f8de
Shovik-2004 <work.shovik@gmail.com>
2024-05-31 20:31:20 +0530

    Initial Commit

---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by shovik banerjee on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
