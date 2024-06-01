8ee41f4b27377c7337ac750bb5ece5a5f51d6fcc
Shovik-2004 <work.shovik@gmail.com>
2024-06-01 19:29:17 +0530

    Initialization

---
+++ JPApexPredators17/ApexPredator.swift
@@ -0,0 +1,8 @@
+//
+//  ApexPredator.swift
+//  JPApexPredators17
+//
+//  Created by shovik banerjee on 31/05/24.
+//
+
+import Foundation
---
+++ JPApexPredators17/Predators.swift
@@ -0,0 +1,8 @@
+//
+//  Predators.swift
+//  JPApexPredators17
+//
+//  Created by shovik banerjee on 01/06/24.
+//
+
+import Foundation
