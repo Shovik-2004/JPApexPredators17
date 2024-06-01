ac2384fd0abdc7526e38a15e0953b62fa3ba47b7
Shovik-2004 <work.shovik@gmail.com>
2024-06-01 19:34:59 +0530

    changed source

--- JPApexPredators17/Predators.swift
+++ JPApexPredators17/Predators.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class Predators{
+    var apexPredators: [ApexPredator] = []
+    
+    func decodeApexPredatorData() {
+        if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json"){
+            do {
+                let data = try Data( contentsOf: url)
+            } catch{
+                print("Error decoding JSON data: \(error)")
+            }
+        }
+    }
+}
