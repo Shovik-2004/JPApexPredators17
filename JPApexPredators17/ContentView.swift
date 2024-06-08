007e4f7199038a72768c1529895ede2fcdff4c03
Shovik-2004 <work.shovik@gmail.com>
2024-06-08 11:06:55 +0530

    Added sorting alphabetically function

--- JPApexPredators17/ContentView.swift
+++ JPApexPredators17/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    let predators = Predators()
+    @State var alphabetical = false
     @State var searchText = ""
+    
+    let predators = Predators()
+    
+    var filteredDinos: [ApexPredator] {
+        predators.sort(by:  alphabetical)
+        return predators.search(for: searchText)
+    }
     
     var body: some View {
         NavigationStack{
-            List(predators.apexPredators) {predator in
+            List(filteredDinos) {predator in
                 NavigationLink{
                     Image(predator.image)
                         .resizable()
@@ -48,6 +55,24 @@ struct ContentView: View {
             }
             .navigationTitle("Apex Predators")
             .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText)
+            .toolbar{
+                ToolbarItem(placement: .topBarLeading){
+                    Button{
+                        withAnimation{
+                            alphabetical.toggle()
+                        }
+                    } label:{
+                        if alphabetical{
+                            Image (systemName: "textformat")
+                            
+                        } else{
+                            Image(systemName: "film")
+                        }
+                    }
+                }
+            }
         }
         .preferredColorScheme(.dark)
     }
